088d0e8c4ec2ee4916eea447ad1d670660e27a3d
ThanhF <thanhnvtps40502@gmail.com>
2026-05-20 20:55:04 +0700

    add analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import Script from 'next/script'
 import './globals.css'
 import type { Metadata } from 'next'
 import type { ReactNode } from 'react'
@@ -15,7 +16,26 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      
+      <body>
+
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-YYVPXWZLF8"
+          strategy="afterInteractive"
+        />
+
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-YYVPXWZLF8');
+          `}
+        </Script>
+        
+        {children}
+      </body>
     </html>
   )
 }
